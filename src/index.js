e8a52879df935dc627c8f4cac84f2832c9282136
Joel Flores <j_flores88@outlook.com>
2024-09-08 20:17:57 -0700

    configure styles in webpack add style.css file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import './style.css';
 
 const component = () => {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'Data Cat'], " ");
+    element.classList.add('hello');
 
     return element;
 };
